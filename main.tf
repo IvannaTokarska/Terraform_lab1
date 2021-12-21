8baf241c1e944fe56a12ccc82bb387f6c17c8e48
IvannaTokarska <78739119+IvannaTokarska@users.noreply.github.com>
2021-12-21 07:34:01 +0200

    main.tf

--- main.tf
+++ main.tf
@@ -4,29 +4,44 @@ provider "google" {
 }
 
 resource "google_compute_network" "vpc_network" {
-  name = "terraform-network"
-  subnet_mode = "custom"
+  name = "vpc-network"
   mtu = 1460 
-  bgp_routing_mode = "regional"
+  auto_create_subnetworks = false
+  project = "tokarska"
+  routing_mode = "regional"
 }
 
 resource "google_compute_network_subnet" "private_subnet_1" {
+  name = "private-subnet-1"
   network = google_compute_network.vpc_network.id
-  range = "10.0.1.0/24"
+  ip_cidr_range = "10.0.1.0/24"
   region = "europe-west1"
 }
 
 resource "google_compute_network_subnet" "private_subnet_2" {
+  name = "private-subnet-2"
   network = google_compute_network.vpc_network.id
-  range = "10.0.2.0/24"
+  ip_cidr_range = "10.0.2.0/24"
   region = "europe-west1"
 }
 
 resource "google_compute_forwarding_rule" "allow_internal_private" {
+  
+  name    = "allow-internal-private"
+  allow {
+    protocol = "ICMP"
+  }
 
   allow {
-      protocol = ["TCP:1-65535", "UDP:1-65535", "ICMP"]
+    protocol = "TCP"
+    ports    = ["1-65535"]
   }
+
+  allow {
+    protocol = "UDP"
+    ports    = ["1-65535"]
+  }
+
   source_ranges = ["10.0.1.0/24", "10.0.2.0/24"]
   network = google_compute_network.vpc_network.id
   priority = 65534
@@ -35,72 +50,57 @@ resource "google_compute_forwarding_rule" "allow_internal_private" {
 resource "google_compute_forwarding_rule" "default_allow_icmp" {
 
   allow {
-      protocol = "ICMP"
+    protocol = "ICMP"
   } 
   network = google_compute_network.vpc_network.id
   priority = 65534
 }
 
-resource "google_compute_forwarding_rule" "allow_lb_health" {
-
-  direction = "INGRESS" 
-  action = "allow" 
-  rules = 80 
-  target_tags = ["allow-healthcheck"]
-  source_ranges = ["130.211.0.0/22", "35.191.0.0/16", "209.85.152.0/22", "209.85.204.0/22"]
-  network = google_compute_network.vpc_network.id
-}
-
-resource "google_compute_forwarding_rule" "allow_http" {
-
-  direction = "INGRESS" 
-  action = "allow" 
-  rules = 80 
-  target_tags = ["allow-http"] 
-  network = google_compute_network.vpc_network.id
-}
-
 resource "google_compute_forwarding_rule" "allow_ssh_ingress_from_iap" {
 
+  name    = "allow-ssh-ingress-from-iap"
   direction = "INGRESS" 
-  action = "allow" 
-  rules = 22
+  allow {
+    protocol = "TCP"
+    ports    = ["22"]
+  }
   source_ranges = "35.235.240.0/20"
   target_tags = ["allow-ssh"] 
   network = google_compute_network.vpc_network.id
 }
 
 resource "google_compute_instance_templates" "backend_template" {
+  
+  name = "backend-template"
   region = "europe-west1"
   network = google_compute_network.vpc_network.id
-  subnet = "private_subnet_2"
-  tags = ["allow-ssh", "allow-healthcheck"] 
-  image_family = "debian-10"
-  image_project = "debian-cloud" 
-  disk {
-      boot = true
-      size = 10
-  } 
+  network_interface {
+    subnetwork = "private-subnet-2"
+  }
+  tags = ["allow-ssh", "no-ip"] 
+  boot_disk {
+    initialize_params {
+      image = "debian-10/debian-cloud"
+    }
+  }
   machine_type = "f1-micro"
 }
 
 resource "google_compute_instance_group_managed" "backend_mig" {
   
+  name = "backend-mig"
   template = "backend_template" 
   size = 2
   zone = ["europe-west1-b", "europe-west1-c"]
   region = "europe-west1"
 }
 
-resource "google_compute_health_checks" "backend_check" {
-  port = 80
-}
-
 resource "google_compute_backend_services" "backend_bs" {
+  
+  name = "backend-bs"
   protocol = "TCP"
   load_balancing_scheme = "INTERNAL"
   network = google_compute_network.vpc_network.id
-  health_checks = google_compute_health_checks.backend_check.id
   region = "europe-west1"
 
   backend {
@@ -113,43 +113,48 @@ resource "google_compute_backend_services" "backend_bs" {
 
 resource "google_compute_forwarding_rule" "backend_lb" {
 
+  name = "backend-lb"
   load_balancing_scheme = "INTERNAL"
   network = google_compute_network.vpc_network.id
+  network_interface {
+    subnetwork = "private-subnet-2"
+  }
   subnet = "private-subnet-2"
   ip_protocol = "TCP" 
   region = "europe-west1"
 }
 
 resource "google_compute_instance_templates" "frontend_template" {
+  
+  name = "frontend-template"
   region = "europe-west1"
   network = google_compute_network.vpc_network.id
-  subnet = "private_subnet_1"
+  network_interface {
+    subnetwork = "private-subnet-1"
+  }
   tags = ["allow-ssh", "allow-healthcheck", "allow-http"] 
-  image_family = "centos-7"
-  image_project = "centos-cloud" 
-  disk {
-      boot = true
-      size = 20
-  } 
+  boot_disk {
+    initialize_params {
+      image = "centos-7/centos-cloud"
+    }
+  }
   machine_type = "f1-micro"
 }
 
 resource "google_compute_instance_group_managed" "frontend_mig" {
   
+  name = "frontend-mig"
   template = "frontend_template" 
   size = 2
   zones = ["europe-west1-b", "europe-west1-c"]
   region = "europe-west1"
 }
 
-resource "google_compute_health_checks" "frontend_check" {
-  port = 80
-}
-
 resource "google_compute_backend_services" "frontend_bs" {
+  
+  name = "frontend-bs"
   protocol = "HTTP"
   port_name = "http"
-  health_checks = google_compute_health_checks.frontend_check.id
 
   backend {
       group = google_compute_instance_group_manager.frontend_mig.instance_group
